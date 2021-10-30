101e9d628d934640ca0c64c7e51950f4041b60c6
Kubafe <Kubafe@DESKTOP-PKQV5KO>
2021-10-30 20:23:37 +0200

    Add project files.

---
+++ TotalCommanderFull/Images.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+//using System.Windows.Forms;
+
+namespace TotalCommanderFull
+{
+   class Images : IDisposable
+   {
+      private string samplePath;
+      private Bitmap sample;
+      private Point lastPosition;
+      private Point startSearch;
+      private static int startX;
+      private static int startY;
+      private Point endSearch;
+      public static string[] buttonNames = new string[] { "one", "two", "three" };
+
+      public static List<Images> imagesList = new List<Images>();
+
+      #region dispose
+      private readonly IntPtr unmanagedResource;
+      private readonly SafeHandle managedResource;
+
+      public void Dispose()
+      {
+         this.Dispose(true);
+         GC.SuppressFinalize(this);
+      }
+
+      protected virtual void Dispose(bool isManualDisposing)
+      {
+         ReleaseUnmanagedResource(unmanagedResource);
+         if (isManualDisposing)
+         {
+            ReleaseManagedResources(managedResource);
+         }
+         // Release
+      }
+
+      private void ReleaseManagedResources(SafeHandle safeHandle)
+      {
+         if (safeHandle != null)
+         {
+            safeHandle.Dispose();
+         }
+      }
+
+      private void ReleaseUnmanagedResource(IntPtr intPtr)
+      {
+         Marshal.FreeHGlobal(intPtr);
+      }
+      #endregion
+
+      /// <summary>
+      /// constructor
+      /// </summary>
+      /// <param name="samplePath">path to sample image</param>
+      /// <param name="startSearch">start searching point in screenshot of sample image</param>
+      /// <param name="endSearch">end searching point in screenshot of sample image</param>
+      public Images(string samplePath, Point startSearch, Point endSearch)
+      {
+         this.samplePath = samplePath;
+         this.startSearch = startSearch;
+         this.endSearch = endSearch;
+         startX = startSearch.X;
+         startY = startSearch.Y;
+         sample = (Bitmap)Image.FromFile(samplePath);
+         lastPosition = new Point(-1, -1);
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      public static bool RecognizeImage(out string imageButtonID, out Point ImagePosition)
+      {
+         int imageNumberID = 0;
+         Point lastPos = new Point(-1, -1);
+         ImagePosition = lastPos;
+         for (int x = 0; x < imagesList.Count; x++)
+         {
+            using (Images sample = imagesList[x])
+            {
+               if (sample.ImageCheck(sample.startSearch, sample.endSearch, ref lastPos))
+               {
+                  imageButtonID = buttonNames[imageNumberID];
+                  ImagePosition = new Point(lastPos.X + startX, lastPos.Y + startY);
+                  sample.Dispose();
+                  return true; //getted image
+               }
+               sample.Dispose();
+               imageNumberID++;
+            }
+         }
+         imageButtonID = string.Empty;
+         lastPos = new Point(-1, -1);
+         return false;
+      }
+
+      /// <summary>
+      /// Check if sample images is appearing on exact screenshot.
+      /// </summary>
+      /// <param name="startSearch">start searching point on display screen</param>
+      /// <param name="endSearch">end searching point on display screen</param>
+      /// <returns>True: sample image is appearing on exact screenshot, False: sample image is not appearing on exact screenshot.</returns>
+      public bool ImageCheck(Point startSearch, Point endSearch, ref Point lastPos)
+      {
+         try
+         {
+            using (Bitmap screen = ExactScreenshot(startSearch, endSearch))
+            {
+               if (lastPosition.X == -1)
+               {
+                  for (int x = 0; x < screen.Width - (sample.Width - 1); x++)
+                  {
+                     for (int y = 0; y < screen.Height - (sample.Height - 1); y++)
+                     {
+                        if (sample.GetPixel(0, 0) == screen.GetPixel(x, y) && IsInnerImage(x, y, sample, screen))
+                        {
+                           lastPosition = new Point(x, y);
+                           lastPos = lastPosition;
+                           screen.Dispose();
+                           return true; //sample is on screen
+                        }
+                     }
+                  }
+                  lastPos = new Point(-1, -1);
+                  screen.Dispose();
+                  return false;
+               }
+               else //lastPosition point of sample image in exact screenshot is saved
+               {
+                  if (!(sample.GetPixel(0, 0) == screen.GetPixel(lastPosition.X, lastPosition.Y) && IsInnerImage(lastPosition.X, lastPosition.Y, sample, screen)))
+                  {
+                     lastPosition = new Point(-1, -1);
+                     ImageCheck(startSearch, endSearch, ref lastPos); //go again and check whole screen - if not appears nowhere - return false;
+                  }
+                  screen.Dispose();
+                  return true;
+               }
+            }
+         }
+         catch (Exception e)
+         {
+            Console.WriteLine($"Images.ImageCheck error: {e.GetType()}");
+            return false;
+         }
+      }
+
+      /// <summary>
+      /// Check if sample is inner image of screen, when first pixel is matched
+      /// </summary>
+      private bool IsInnerImage(int left, int top, Bitmap sample, Bitmap screen)
+      {
+         for (int x = 0; x < sample.Width; x++)
+         {
+            for (int y = 0; y < sample.Height; y++)
+            {
+               if (sample.GetPixel(x, y) != screen.GetPixel(left + x, top + y))
+               {
+                  return false; //sample is not inner image of screen
+               }
+            }
+         }
+         return true; //sample is inner image of screen
+      }
+
+      /// <summary>
+      /// Get screenshot of exact area on screen.
+      /// </summary>
+      /// <param name="startScreen">start point of screen (left, up)</param>
+      /// <param name="endScreen">end point of screen (right, down)</param>
+      /// <returns>screenshot bitmap</returns>
+      private Bitmap ExactScreenshot(Point startScreen, Point endScreen)
+      {
+         Size size = new Size(endScreen.X - startScreen.X, endScreen.Y - startScreen.Y);
+         Bitmap screenshot = new Bitmap(size.Width, size.Height);
+         Graphics gfx = Graphics.FromImage(screenshot);
+         gfx.CopyFromScreen(startScreen.X, startScreen.Y, 0, 0, size);
+         return screenshot;
+      }
+
+   }
+}
---
+++ TotalCommanderFull/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TotalCommanderFull
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         //add images to list:
+         for (int x = 1; x <= 3; x++) 
+         {
+            Images.imagesList.Add(new Images(Path.Combine(Application.StartupPath, $"{Images.buttonNames[x - 1]}.png"), new Point(650, 460), new Point(700, 500)));
+         }
+         //start total commander process:
+         Process process = new Process();
+         process.StartInfo.FileName = @"c:\program files\totalcmd\TOTALCMD64.EXE";
+         process.Start();
+         process.WaitForInputIdle();
+         //recognize image:
+         string buttonName;
+         int repetition = 0;
+         Point imagePosition;
+         while (!Images.RecognizeImage(out buttonName, out imagePosition) && repetition < 30) //3 sec waiting (30x100ms)
+         {
+            repetition++;
+            System.Threading.Thread.Sleep(100);
+         }
+         ClickCorrectButton(buttonName, imagePosition);
+
+         //next - settings for images, search location, ... - second winform app -> db or file for both programs
+
+      }
+
+      private static void ClickCorrectButton(string buttonName, Point imagePosition)
+      {
+         int xMargin = 110;
+         int yMargin = 32;
+         switch (buttonName)
+         {
+            default:
+               {
+                  Console.WriteLine("Žádný image sample nenalezen.");
+                  break;
+               }
+            case "one": //potom asi dictionary<string,point>
+               {
+                  LMBclick(new Point(imagePosition.X - xMargin, imagePosition.Y + yMargin), true);
+                  break;
+               }
+            case "two":
+               {
+                  LMBclick(new Point(imagePosition.X, imagePosition.Y + yMargin), true);
+                  break;
+               }
+            case "three":
+               {
+                  LMBclick(new Point(imagePosition.X + xMargin, imagePosition.Y + yMargin), true);
+                  break;
+               }
+         }
+      }
+
+      #region HandleMouse
+      [DllImport("user32.dll")]
+      public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+      public enum MouseEventFlags
+      {
+         LEFTDOWN = 0x00000002,
+         LEFTUP = 0x00000004,
+         MIDDLEDOWN = 0x00000020,
+         MIDDLEUP = 0x00000040,
+         RIGHTDOWN = 0x00000008,
+         RIGHTUP = 0x00000010
+      }
+
+      /// <summary>
+      /// Send left mouse click on selected position.
+      /// </summary>
+      /// <param name="p">Point for position to send left mouse click.</param>
+      public static void SendLeftMouse(Point p)
+      {
+         Cursor.Position = p;
+         LMBclick();
+      }
+
+      /// <summary>
+      /// Send left mouse click.
+      /// </summary>
+      public static void LMBclick()
+      { mouse_event((int)MouseEventFlags.LEFTDOWN | (int)MouseEventFlags.LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0); }
+
+      /// <summary>
+      /// Send left mouse click on selected position and take back cursor to last position if selected.
+      /// </summary>
+      /// <param name="point">Point for position to send left mouse click.</param>
+      /// <param name="lastPos">Take back cursor to lastPosition before left mouse click. True: take back cursor to last position, Default: false (not take back cursor to last position).</param>
+      public static void LMBclick(Point point, bool lastPos = false)
+      {
+         Point lastPosition = Cursor.Position;
+         Cursor.Position = point;
+         mouse_event((int)MouseEventFlags.LEFTDOWN | (int)MouseEventFlags.LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+         if (lastPos)
+         {
+            Cursor.Position = lastPosition;
+         }
+      }
+      /// <summary>
+      /// Send middle mouse click.
+      /// </summary>
+      public static void MMBclick()
+      { mouse_event((int)MouseEventFlags.MIDDLEDOWN | (int)MouseEventFlags.MIDDLEUP, Cursor.Position.X, Cursor.Position.Y, 0, 0); }
+      /// <summary>
+      /// Send right mouse click.
+      /// </summary>
+      public static void RMBClick()
+      { mouse_event((int)MouseEventFlags.RIGHTDOWN | (int)MouseEventFlags.RIGHTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0); }
+      #endregion
+
+   }
+}
